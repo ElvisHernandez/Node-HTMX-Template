70f3ba3cddea0af190a08e2793c96195b3823e35
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-11-08 01:00:15 -0500

    configure tailwaind, daisy ui, htmx, eslint, prettier and sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
 import express from "express";
+import { Db } from "./services/Db";
+import hbs from "hbs";
+import path from "path";
+
+hbs.registerPartials(path.resolve(__dirname, "./views/partials"));
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.send("<h1>hello, thing</h1>");
+const db = Db.getInstance();
+
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
+app.use(express.static(path.resolve(__dirname, "../static")));
+app.use((req, res, next) => {
+	req.db = db;
+	next();
+});
+
+app.get("/admin", async (req, res) => {
+	res.render("admin", {
+		message: "ding dong",
+	});
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{hbs}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+};
